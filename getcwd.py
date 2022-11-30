948df03fe378f48af2512490d1d1bffa7fe43bc0
xiaoxiami618 <caokun618@163.com>
2022-11-30 19:59:48 +0800

    hello to github

---
+++ getcwd.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 19:58:50 2022
+
+@author: caokun
+"""
+
+import os
+
+print(os.getcwd())
---
+++ hellogithub.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 19:47:06 2022
+
+@author: caokun
+"""
+
+print('hello github')
+print('hello haha')
